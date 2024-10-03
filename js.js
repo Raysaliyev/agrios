ef4872221f4f9bb01bb95bc51a2853ff86afe10b
Umidjon_developer <127392774+Raysaliyev@users.noreply.github.com>
2024-10-03 15:10:26 +0500

    malumot qoyildi

---
+++ js.js
@@ -0,0 +1,10 @@
+const my ={
+    1:'Umidjon',
+    7:'Raysaliyev',
+    9:1.80,
+    100:'Full dasturchi',
+    10:'BMW',
+ 
+}
+
+delete my.buy;console.log(Object.keys(my));
---
+++ js1.js
@@ -0,0 +1,115 @@
+
+//1
+
+// let a = +prompt('son kirting');
+// let b = +prompt('son  kirting');
+
+// let j = a+b;
+// alert(j**2);
+
+
+//2
+
+// let a = 12>7;
+// let b = 12<7;
+
+// let javob = a || b
+// alert(javob);
+// let ja = a && b;
+// alert(ja);
+
+//3
+
+// let a = +prompt('son krting');
+// let b = +prompt('son krting');
+
+// let javob = a/b;
+// if (javob%2 == 0) {
+   
+// }else{
+//     let ja = javob**3;
+//     alert(ja);
+// }
+
+//4
+
+// let my = +prompt('kundi krting');
+
+// switch (my) {
+//     case 1:
+//         alert('Dushanba'); // Monday
+//         break;
+//     case 2:
+//         alert('Seshanba'); // Tuesday
+//         break;
+//     case 3:
+//         alert('Chorshanba'); // Wednesday
+//         break;
+//     case 4:
+//         alert('Payshanba'); // Thursday
+//         break;
+//     case 5:
+//         alert('Juma'); // Friday
+//         break;
+//     case 6:
+//         alert('Shanba'); // Saturday
+//         break;
+//     case 7:
+//         alert('Yakshanba'); // Sunday
+//         break;
+//     default:
+
+//         alert('Noto\'g\'ri kun raqami!'); // If the value is not valid
+// }
+
+
+
+//5
+// if (kino <= 16) {
+//     alert('multik')
+// }else if(kino > 16 && kino <= 22){
+//     alert('kamidya kino')
+// }else if(kino >=22 && kino <=28){
+//     alert('dedigtif')
+// }else if(kino >28 && kino <=40){
+//     alert('beznes')
+// }else{
+//     alert('axporot')
+// }
+
+// 6
+
+// for (let i = 0; i<10; i++) {
+//     let jav= i**2;
+//     alert(jav);
+    
+// }
+
+
+let myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+console.log(myArray.length);
+
+myArray[70] = 'Umidjon Raysaliyev'
+console.log(myArray);
+
+let myArray2 = ['Umidjjon', 'Otabek', 'Muhammad']
+
+myArray2.push('Umar')
+console.log(myArray2);
+
+myArray2.pop()
+console.log(myArray2);
+
+
+myArray2.unshift('Salomat')
+console.log(myArray2);
+
+
+myArray2.shift()
+console.log(myArray2);
+
+
+
+
+ 
